bf5c7b7f1fad93fb4babd5eafaeedfd1fffa1f5e
phofmeier <phofmeier@googlemail.com>
2026-03-15 12:29:54 +0000

    add some documentation

--- components/driver_gp8211s/driver_gp8211s.c
+++ components/driver_gp8211s/driver_gp8211s.c
@@ -4,43 +4,32 @@
 #include "driver/i2c_master.h"
 #include <stdio.h>
 
-// https://esp32.com/viewtopic.php?t=46042
-
-// https://github.com/DFRobot/DFRobot_GP8XXX/blob/master/DFRobot_GP8XXX.h
-
 i2c_master_bus_handle_t i2c_bus_handle;
 i2c_master_dev_handle_t gp8211s_dev_handle;
 
-#define DEVICE_ADDRESS 0x58 // or 0x59 Needs to be checked
-#define OUTPUT_RANGE_5V_DATA 0x00
-#define OUTPUT_RANGE_10V_DATA 0x11 // or 0x77
+// Datasheet: https://jlcpcb.com/partdetail/Guestgood-GP8211S_TC50EW/C3152008
 
-#define RESOLUTION_15_BIT 0x7FFF
-#define CONFIG_REGISTER 0x01
-#define OUTPUT_VALUE_REGISTER 0x02
+#define DEVICE_ADDRESS 0x58 // Datasheet says 0x58
+// but maybe 0x59 from some strange internet source.
+// https://esp32.com/viewtopic.php?t=46042
 
-void gp8211s_init_i2c() {
-  // TODO: Implement I2C initialization for GP8211S
+// Following values are from the DFRobot Library, but datasheet seems to suggest
+// different values. Needs proper testing
+// https://github.com/DFRobot/DFRobot_GP8XXX/blob/master/DFRobot_GP8XXX.h
+#define OUTPUT_RANGE_5V_DATA 0x00  // Datasheet says 0x55
+#define OUTPUT_RANGE_10V_DATA 0x11 // Datasheet says 0x77
 
-  // Create I2C master handle
-  //   i2c_master_bus_config_t i2c_master_config = {
-  //       .i2c_port = 1,
-  //       .sda_io_num = (gpio_num_t)PIN_I2C_SDA,
-  //       .scl_io_num = (gpio_num_t)PIN_I2C_SCL,
-  //       .clk_source = I2C_CLK_SRC_DEFAULT,
-  //       .glitch_ignore_cnt = 7,
-  //       .intr_priority = 1,
-  //       .trans_queue_depth = 10,
-  //       .flags = {.enable_internal_pullup = 1, .allow_pd = 0}};
-  //   ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_master_config,
-  //   &i2c_master_handle)); if (i2c_master_handle == NULL) {
-  //     ESP_LOGE(TAG, "Error creating I2C master handle");
-  //   }
+#define CONFIG_REGISTER 0x01       // Register address to set output range
+#define OUTPUT_VALUE_REGISTER 0x02 // Register address to set output value
+#define DEVICE_SCL_SPEED_HZ 100000 // Datasheet says max 400kHz
+
+// Resolution is 15 bit. Bit 16 needs to be 0
 
 #define I2C_MASTER_PORT I2C_NUM_0
-#define I2C_MASTER_SCL_IO 4
-#define I2C_MASTER_SDA_IO 5
+#define I2C_MASTER_SCL_IO 22
+#define I2C_MASTER_SDA_IO 21
 
+void gp8211s_init_i2c() {
   i2c_master_bus_config_t i2c_mst_config = {
       .clk_source = I2C_CLK_SRC_DEFAULT,
       .i2c_port = I2C_MASTER_PORT,
@@ -57,7 +46,7 @@ void gp8211s_init_device() {
   i2c_device_config_t dev_cfg = {
       .dev_addr_length = I2C_ADDR_BIT_LEN_7,
       .device_address = DEVICE_ADDRESS,
-      .scl_speed_hz = 100000,
+      .scl_speed_hz = DEVICE_SCL_SPEED_HZ,
       .scl_wait_us = 0,
   };
 
