840d34048e8ff6e7202ca6bba05b327f3d6df609
phofmeier <phofmeier@googlemail.com>
2026-05-07 13:55:32 +0000

    median filter measurements

--- components/driver_hc_sr04/driver_hc_sr04.c
+++ components/driver_hc_sr04/driver_hc_sr04.c
@@ -10,6 +10,8 @@
 #define WAIT_FOR_ECHO_LOW_TIMEOUT_US 1000000  // 1 second timeout
 #define SONIC_SPEED_CM_PER_S 34320.0          // Speed of sound in cm/s at 20 °C
 
+static const char *TAG = "hc_sr04";
+
 static portMUX_TYPE hc_sr04_critical_section_mutex =
     portMUX_INITIALIZER_UNLOCKED;
 
@@ -40,11 +42,17 @@ void hc_sr04_init() {
 esp_err_t hc_sr04_get_distance_cm(float *distance_cm) {
   taskENTER_CRITICAL(&hc_sr04_critical_section_mutex);
   // Set trigger to high
-  gpio_set_level(CONFIG_HC_SR04_TRIGGER_PIN, 1);
+  ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_set_level(CONFIG_HC_SR04_TRIGGER_PIN, 1));
   // wait for 10 micro seconds
-  esp_rom_delay_us(10);
+  esp_rom_delay_us(11); // Also seen code with 5 us
   // set trigger to low
-  gpio_set_level(CONFIG_HC_SR04_TRIGGER_PIN, 0);
+  ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_set_level(CONFIG_HC_SR04_TRIGGER_PIN, 0));
+
+  if (gpio_get_level(CONFIG_HC_SR04_ECHO_PIN) == 1) {
+    taskEXIT_CRITICAL(&hc_sr04_critical_section_mutex);
+    ESP_LOGE(TAG, "Echo pin is already high after triggering measurement");
+    return ESP_FAIL; // Echo pin should be low after triggering
+  }
 
   const int start_waiting_time = esp_timer_get_time();
   while (gpio_get_level(CONFIG_HC_SR04_ECHO_PIN) == 0) {
@@ -52,6 +60,7 @@ esp_err_t hc_sr04_get_distance_cm(float *distance_cm) {
     if (esp_timer_get_time() - start_waiting_time >
         WAIT_FOR_ECHO_HIGH_TIMEOUT_US) {
       taskEXIT_CRITICAL(&hc_sr04_critical_section_mutex);
+      ESP_LOGE(TAG, "Timeout waiting for echo to go high");
       return ESP_ERR_TIMEOUT; // Timeout waiting for echo to go high
     }
   }
@@ -60,6 +69,7 @@ esp_err_t hc_sr04_get_distance_cm(float *distance_cm) {
     // wait for echo to go low with timeout
     if (esp_timer_get_time() - echo_start_time > WAIT_FOR_ECHO_LOW_TIMEOUT_US) {
       taskEXIT_CRITICAL(&hc_sr04_critical_section_mutex);
+      ESP_LOGE(TAG, "Timeout waiting for echo to go low");
       return ESP_ERR_TIMEOUT; // Timeout waiting for echo to go low
     }
   }
@@ -70,6 +80,7 @@ esp_err_t hc_sr04_get_distance_cm(float *distance_cm) {
   *distance_cm =
       ((echo_end_time - echo_start_time) / 1e6) * SONIC_SPEED_CM_PER_S / 2.0;
   if (*distance_cm < 2.0 || *distance_cm > 400) {
+    ESP_LOGI(TAG, "Measured distance out of range: %.2f cm", *distance_cm);
     return ESP_FAIL;
   }
 
