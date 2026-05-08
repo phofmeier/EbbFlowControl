28100be93cfeaf31870f3429bb6a2c8134ee95e3
phofmeier <phofmeier@googlemail.com>
2026-05-08 09:44:30 +0000

    measure directly in mm

--- components/driver_hc_sr04/include/driver_hc_sr04.h
+++ components/driver_hc_sr04/include/driver_hc_sr04.h
@@ -13,10 +13,10 @@ void hc_sr04_init();
  * @brief Get distance measurement from the HC-SR04 sensor.
  *
  * This function triggers a measurement and waits for the echo signal to
- * determine the distance. The distance is returned in centimeters.
+ * determine the distance. The distance is returned in millimeters.
  *
- * @return Distance in centimeters, or -1 if an error occurred (e.g., timeout).
+ * @return Distance in millimeters, or -1 if an error occurred (e.g., timeout).
  */
-esp_err_t hc_sr04_get_distance_cm(float *distance_cm);
+esp_err_t hc_sr04_get_distance_mm(float *distance_mm);
 
 #endif /* COMPONENTS_DRIVER_HC_SR04_INCLUDE_DRIVER_HC_SR04 */
